9120280969ce77331f264fc70694993e0536ef5d
Matheus Soares <matheups480@gmail.com>
2022-10-10 01:31:50 -0300

    Special move - En Passant

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -22,6 +22,7 @@ public class ChessMatch {  //classe dedicada às regras do sistema de xadrez
 	private Board board;
 	private boolean check;
 	private boolean checkMate;
+	private ChessPiece enPassantVulnerable;
 	
 	private List<Piece> piecesOnTheBoard = new ArrayList<>();
 	private List<Piece> capturedPieces = new ArrayList<>();
@@ -57,6 +58,10 @@ public class ChessMatch {  //classe dedicada às regras do sistema de xadrez
 	
 	public boolean getCheckMate() {
 		return checkMate;
+	}
+	
+	public ChessPiece getEnPassantVulnerable() {
+		return enPassantVulnerable;
 	}
 	
 	//METODOS
@@ -98,6 +103,10 @@ public class ChessMatch {  //classe dedicada às regras do sistema de xadrez
 			throw new ChessExceptions("You can´t put yourself in check");
 		} 
 		
+		ChessPiece movedPiece = (ChessPiece)board.piece(target);
+		
+		
+		
 		check = (testCheck(opponent(currentPlayer))) ? true : false; 
 		
 		if(testCheckMate(opponent(currentPlayer))) {
@@ -106,6 +115,18 @@ public class ChessMatch {  //classe dedicada às regras do sistema de xadrez
 		
 		nextTurn();
 	}
+		
+		//movimento especial enPassant
+		
+		if(movedPiece instanceof Pawn && (target.getRow() == source.getRow() - 2 || target.getRow() == source.getRow() + 2 )) {  //identifica se foi feito um movimento inicial de peão, deixando vulneravel para o enPassant
+			
+			enPassantVulnerable = movedPiece;
+			
+		}
+		else {
+			
+			enPassantVulnerable = null;
+		}
 		return (ChessPiece) capturedPiece;
 		
 	} 
@@ -144,6 +165,25 @@ public class ChessMatch {  //classe dedicada às regras do sistema de xadrez
 
 		}
 		
+		//movimento especial enPassant
+		
+		if(p instanceof Pawn) {
+			if(source.getColumn() != target.getColumn() && capturedPiece == null) {
+				Position pawnPosition;
+				if(p.getColor() == Color.WHITE) {
+					
+					pawnPosition = new Position(target.getRow() + 1, target.getColumn()); 
+				}
+				else {
+					pawnPosition = new Position(target.getRow() - 1, target.getColumn());
+				}
+				capturedPiece = board.removePiece(pawnPosition);
+				capturedPieces.add(capturedPiece);
+				piecesOnTheBoard.remove(capturedPiece);
+			}
+			
+		}
+		
 		return capturedPiece;
 	}
 	
@@ -180,6 +220,25 @@ public class ChessMatch {  //classe dedicada às regras do sistema de xadrez
 			ChessPiece rook = (ChessPiece) board.removePiece(targetT); // tira a torre de onde ela está
 			board.placePiece(rook, sourceT);
 			rook.decreaseMoveCount();
+
+		}
+		
+		//movimento especial enPassant
+		
+		if (p instanceof Pawn) {
+			if (source.getColumn() != target.getColumn() && capturedPiece == enPassantVulnerable) {
+				ChessPiece pawn = (ChessPiece)board.removePiece(target);
+				Position pawnPosition;
+				if (p.getColor() == Color.WHITE) {
+
+					pawnPosition = new Position(3, target.getColumn());
+				} else {
+					pawnPosition = new Position(4, target.getColumn());
+				}
+				
+				board.placePiece(pawn, pawnPosition);
+				capturedPieces.add(capturedPiece);
+			}
 
 		}
 	}
@@ -302,14 +361,14 @@ public class ChessMatch {  //classe dedicada às regras do sistema de xadrez
         placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('g', 1, new Knight(board, Color.WHITE));
         placeNewPiece('h', 1, new Rook(board, Color.WHITE));
-        placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
-        placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
-        placeNewPiece('c', 2, new Pawn(board, Color.WHITE));
-        placeNewPiece('d', 2, new Pawn(board, Color.WHITE));
-        placeNewPiece('e', 2, new Pawn(board, Color.WHITE));
-        placeNewPiece('f', 2, new Pawn(board, Color.WHITE));
-        placeNewPiece('g', 2, new Pawn(board, Color.WHITE));
-        placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('a', 2, new Pawn(board, Color.WHITE, this));
+        placeNewPiece('b', 2, new Pawn(board, Color.WHITE, this));
+        placeNewPiece('c', 2, new Pawn(board, Color.WHITE, this));
+        placeNewPiece('d', 2, new Pawn(board, Color.WHITE, this));
+        placeNewPiece('e', 2, new Pawn(board, Color.WHITE, this));
+        placeNewPiece('f', 2, new Pawn(board, Color.WHITE, this));
+        placeNewPiece('g', 2, new Pawn(board, Color.WHITE, this));
+        placeNewPiece('h', 2, new Pawn(board, Color.WHITE, this));
 
         placeNewPiece('a', 8, new Rook(board, Color.BLACK));
         placeNewPiece('b', 8, new Knight(board, Color.BLACK));
@@ -319,14 +378,14 @@ public class ChessMatch {  //classe dedicada às regras do sistema de xadrez
         placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('g', 8, new Knight(board, Color.BLACK));
         placeNewPiece('h', 8, new Rook(board, Color.BLACK));
-        placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
-        placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
-        placeNewPiece('c', 7, new Pawn(board, Color.BLACK));
-        placeNewPiece('d', 7, new Pawn(board, Color.BLACK));
-        placeNewPiece('e', 7, new Pawn(board, Color.BLACK));
-        placeNewPiece('f', 7, new Pawn(board, Color.BLACK));
-        placeNewPiece('g', 7, new Pawn(board, Color.BLACK));
-        placeNewPiece('h', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('a', 7, new Pawn(board, Color.BLACK, this));
+        placeNewPiece('b', 7, new Pawn(board, Color.BLACK, this));
+        placeNewPiece('c', 7, new Pawn(board, Color.BLACK, this));
+        placeNewPiece('d', 7, new Pawn(board, Color.BLACK, this));
+        placeNewPiece('e', 7, new Pawn(board, Color.BLACK, this));
+        placeNewPiece('f', 7, new Pawn(board, Color.BLACK, this));
+        placeNewPiece('g', 7, new Pawn(board, Color.BLACK, this));
+        placeNewPiece('h', 7, new Pawn(board, Color.BLACK, this));
 	
 	}
 	
